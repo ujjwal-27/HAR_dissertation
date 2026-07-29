e538e0dc67a4c451c407d424b4bb3a64fc218f01
Ujjwal Shrestha <ujjwalshresth.96@gmail.com>
2026-07-29 18:53:42 +0100

    create constants

---
+++ src/config/constants.py
@@ -0,0 +1,47 @@
+"""
+File: constants.py
+
+Description:
+    Defines project-wide constant values used throughout the
+    dissertation implementation.
+
+Author:
+    Ujjwal Shrestha
+
+Dissertation:
+    A Comparative Study of Transfer Learning-Based CNN Architectures
+    for Image-Based Human Activity Recognition
+"""
+
+# --------------------------------------------------
+# Dataset
+# --------------------------------------------------
+
+# Dataset name
+DATASET_NAME = "Stanford40"
+
+# Total number of activity classes
+NUM_CLASSES = 40
+
+# Supported image file extensions
+IMAGE_EXTENSIONS = (
+    ".jpg",
+    ".jpeg",
+)
+
+# --------------------------------------------------
+# Image Processing
+# --------------------------------------------------
+
+# Target image size for pretrained CNN models
+TARGET_IMAGE_SIZE = (224, 224)
+
+# Image colour mode
+RGB_MODE = "RGB"
+
+# --------------------------------------------------
+# Reproducibility
+# --------------------------------------------------
+
+# Random seed
+RANDOM_SEED = 42
