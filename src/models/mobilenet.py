69d0a757dae2d5b18db6ee8ab4e6260da93492f2
Ujjwal Shrestha <ujjwalshresth.96@gmail.com>
2026-08-07 21:35:58 +0100

    create mobilenet model

---
+++ src/models/mobilenet.py
@@ -0,0 +1,50 @@
+"""
+File: mobilenet.py
+
+Description:
+    Implements the pretrained MobileNetV3-Large model for
+    feature extraction.
+
+Author:
+    Ujjwal Shrestha
+
+Dissertation:
+    A Comparative Study of Transfer Learning-Based CNN Architectures
+    for Image-Based Human Activity Recognition
+"""
+
+import torch.nn as nn
+from torchvision.models import (
+    MobileNet_V3_Large_Weights,
+    mobilenet_v3_large,
+)
+
+from src.config.constants import NUM_CLASSES
+
+
+def build_mobilenet() -> nn.Module:
+    """
+    Build a pretrained MobileNetV3-Large model for feature extraction.
+
+    Returns
+    -------
+    nn.Module
+        MobileNetV3-Large model.
+    """
+
+    # Load pretrained MobileNetV3-Large
+    model = mobilenet_v3_large(
+        weights=MobileNet_V3_Large_Weights.DEFAULT,
+    )
+
+    # Freeze all pretrained layers
+    for parameter in model.parameters():
+        parameter.requires_grad = False
+
+    # Replace classifier
+    model.classifier[-1] = nn.Linear(
+        model.classifier[-1].in_features,
+        NUM_CLASSES,
+    )
+
+    return model
